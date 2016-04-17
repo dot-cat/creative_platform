fc555bc7b7a069e14c6550f8c69330cfeebe7c8e
Sergey Kostyuk <ks.hot.ua@gmail.com>
2016-04-17 18:47:27 +0300

    WIP: another version of outputs class

--- outputs/outputs.py
+++ outputs/outputs.py
@@ -12,13 +12,19 @@ sclr = 40  # пин для очистки
 
 WorkRegistr = ShiftRegister(si, sck, rck, sclr)
 
+
+class PinData(object):
+    def __init__(self, pin_number, value):
+        self.pin_number = pin_number
+        self.value = value
+        return 
+    
+
 class Outputs(object):
     # Константы:
     ON  = True
     OFF = False
     ACTIONS = [ON, OFF]
-    ROOMS = ['name', 'name2']
-
 
     def __init__(self):
         """
@@ -26,75 +32,58 @@ class Outputs(object):
         :return: none
         """
 
+        self.door_1_plus  = 0
+        self.door_1_minus = 1
+        self.door_2_plus  = 2
+        self.door_2_minus = 3
+        self.diode_1 = 4
+        self.diode_2 = 5
+        self.door_3_plus  = 6
+        self.door_3_minus = 7
+        self.door_4_plus  = 8
+        self.door_4_minus = 9
+        self.diode_3 = 10
+        self.diode_4 = 11
+        self.blind_1_plus  = 12
+        self.blind_1_minus = 13
+        self.blind_2_plus  = 14
+        self.blind_2_minus = 15
+        self.blind_3_plus  = 16
+        self.blind_3_minus = 17
+        self.diode_5 = 18
+        self.diode_6 = 19
+        self.cooler  = 20
 
-        self.openfirstdoor = 0x2 #равносильно 0b10
-        self.closefirstdoor = 0x1#равносильно 0b01
+        self.current_state = 0x0
 
-        self.openseconddoor = 0x8
-        self.closeseconddoor = 0x4
+        self.control_Door_dict = {'Open first door':   [PinData(self.door_1_plus, 1), PinData(self.door_1_minus, 0)],
+                                  'Close first door':  [PinData(self.door_1_plus, 0), PinData(self.door_1_minus, 1)],
+                                  'Open second door':  [PinData(self.door_2_plus, 1), PinData(self.door_2_minus, 0)],
+                                  'Close second door': [PinData(self.door_2_plus, 0), PinData(self.door_2_minus, 1)],
+                                  'Open third door':   [PinData(self.door_3_plus, 1), PinData(self.door_3_minus, 0)],
+                                  'Close third door':  [PinData(self.door_3_plus, 0), PinData(self.door_3_minus, 1)],
+                                  'Open fourth door':  [PinData(self.door_4_plus, 1), PinData(self.door_4_minus, 0)],
+                                  'Close fourth door': [PinData(self.door_4_plus, 0), PinData(self.door_4_minus, 1)]
+                                  }
 
-        self.openthirddoor = 0x80
-        self.closethirddoor = 0x40
+        self.room_led_dict = {'room 1':  self.diode_1,
+                              'room 2':  self.diode_2,
+                              'room 3':  self.diode_3,
+                              'room 4':  self.diode_4,
+                              'room 5':  self.diode_5,
+                              'room 6':  self.diode_6
+                              }
 
-        self.openfourthdoor = 0x200
-        self.closefourthdoor = 0x100
+        self.control_Cooler_dict = {'On cooler':  PinData(self.cooler, 1),
+                                    'Off cooler': PinData(self.cooler, 0)}
 
-
-
-        self.firstLED = 0x10
-        self.secondLED = 0x20
-        self.thirdLED = 0x400
-        self.fourthLED = 0x800
-        self.fifthLED = 0x40000
-        self.sixthLED = 0x80000
-
-
-        self.cooler = 0x200000
-
-
-        self.openfirstblind = 0x2000
-        self.closefirstblind = 0x1000
-
-        self.opensecondblind = 0x8000
-        self.closesecondblind = 0x4000
-
-        self.openthirdblind = 0x20000
-        self.closethirdblind = 0x10000
-
-        self.currentstate = 0x0
-
-        self.control_Door_dict = {'Open first door':self.openfirstdoor,
-                           'Close first door':self.closefirstdoor,
-                            'Open second door': self.openseconddoor,
-                            'Close second door': self.closeseconddoor,
-                            'Open third door': self.openthirddoor,
-                            'Close third door': self.closethirddoor,
-                            'Open fourth door': self.openfourthdoor,
-                            'Close fourth door': self.closefourthdoor}
-
-        self.controlLED_dict = {'On first LED':self.firstLED,
-                                 'Off first LED': self.firstLED,
-                                 'On second LED':self.secondLED,
-                                 'Off second LED': self.secondLED,
-                                 'On third LED': self.thirdLED,
-                                 'OFF third LED': self.thirdLED,
-                                 'On Fourth LED': self.fourthLED,
-                                 'Off Fourth LED': self.fourthLED,
-                                 'On fifth LED': self.fifthLED,
-                                 'Off fifth LED': self.fifthLED,
-                                 'On sixth LED': self.sixthLED,
-                                 'Off sixth LED':self.sixthLED}
-
-        self.control_Cooler_dict = {'On cooler': self.cooler,
-                                  'Off cooler': self.cooler}
-
-
-        self.control_Blind_dict = {'Open first blind': self.openfirstblind,
-                                 'Close first blind': self.closefirstblind,
-                                 'Open second blind': self.opensecondblind,
-                                 'Close second blind': self.closesecondblind,
-                                 'Open third blind': self.openthirdblind,
-                                 'Close third blind': self.closethirdblind}
+        self.control_Blind_dict = {'Open first blind':   [PinData(self.blind_1_plus, 1), PinData(self.blind_1_minus, 0)],
+                                   'Close first blind':  [PinData(self.blind_1_plus, 0), PinData(self.blind_1_minus, 1)],
+                                   'Open second blind':  [PinData(self.blind_2_plus, 1), PinData(self.blind_1_minus, 0)],
+                                   'Close second blind': [PinData(self.blind_2_plus, 0), PinData(self.blind_1_minus, 1)],
+                                   'Open third blind':   [PinData(self.blind_3_plus, 1), PinData(self.blind_1_minus, 0)],
+                                   'Close third blind':  [PinData(self.blind_3_plus, 0), PinData(self.blind_1_minus, 1)]
+                                   }
 
 
         return
@@ -105,7 +94,7 @@ class Outputs(object):
         :return: none
         """
         self.control_Door_dict.clear()
-        self.controlLED_dict.clear()
+        self.room_led_dict.clear()
         self.control_Cooler_dict.clear()
         self.control_Blind_dict.clear()
         return
@@ -119,14 +108,14 @@ class Outputs(object):
         if door_id != str:
             raise ValueError('Value must be a string literal')
 
-        if door_id not in self.control_Door_dict():
+        if door_id not in self.control_Door_dict:
             raise ValueError('Value not found')
 
-        RightDoor = self.control_Door_dict.get(door_id)
-        WorkRegistr.write_data(RightDoor)
-
-
-        return
+        door_data = self.control_Door_dict.get(door_id)
+        self.set_bit(door_data[0].pin_number, door_data[0].value)
+        self.set_bit(door_data[1].pin_number, door_data[1].value)
+        WorkRegistr.write_data(self.current_state)
+        return True
 
     def turn_light(self, room_name, light_action):
         """
@@ -144,9 +133,33 @@ class Outputs(object):
         elif light_action not in self.ACTIONS:
             raise ValueError('wrong action with lights')
 
+        elif room_name not in self.room_led_dict:
+            raise ValueError('Value not found')
 
-        # TODO: написать код и убрать NotImplementedError
+        led_data = self.room_led_dict.get(room_name)
 
+        if light_action == self.ON:
+            self.set_bit(led_data.pin_number, 1)
+        elif light_action == self.OFF:
+            self.set_bit(led_data.pin_number, 0)
+        else:
+            raise ValueError('Unknown action')
 
-        raise NotImplementedError('Not implemented')
-        return False
+        WorkRegistr.write_data(self.current_state)
+
+        return True
+
+    def set_bit(self, bit_num, value):
+        if bit_num < 0:
+            raise ValueError('Bit number must be positive or zero')
+
+        if value != 0 or value != 1:
+            raise ValueError('Value must be 1 or zero, True or False')
+
+        if value == 0:
+            self.current_state &= ~(1 << bit_num)
+
+        else:
+            self.current_state |= (1 << bit_num)
+
+        return
