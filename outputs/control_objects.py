baf5c9452b1c9395fb1f68dac4f9a63f426a1466
Sergey Kostyuk <ks.hot.ua@gmail.com>
2016-04-23 20:42:20 +0300

    WIP: Finished base for control_objects

--- outputs/control_objects.py
+++ outputs/control_objects.py
@@ -3,6 +3,12 @@
 
 from shift_reg_wrapper import ShiftRegWrapper
 from enum import Enum
+import time
+
+
+def check_shift_reg_type(shift_reg):
+    if type(shift_reg) != ShiftRegWrapper:
+        raise ValueError('type of shift_reg value must be a ShiftRegWrapper')
 
 
 class Trigger(object):
@@ -26,19 +32,28 @@ class Trigger(object):
         :param bit_pos: позиция бита, который контролирует объект
         :return: None
         """
+        check_shift_reg_type(shift_reg)
+
         self.shift_reg = shift_reg
         self.bit_pos = bit_pos
 
-    def set_state(self, state):
+    def get_state(self):
+        """
+        Получение текущего состояния переключателя
+        :return: значение типа Trigger.States
+        """
+        return self.States(self.shift_reg.get_buf_bit(self.bit_pos))
+
+    def __set_state(self, state):
         """
         Установка состояния объекта и запись состояния в буфер
         :param state: желаемое состояние
         :return: None
         """
-        assert Trigger.States.on == True and Trigger.States.off == False,\
+        assert self.States.on == True and self.States.off == False,\
             'state code changed, update this function before usage'
 
-        if type(state) != Trigger.States:
+        if type(state) != self.States:
             raise ValueError('Type of state argument must be a Trigger.State')
 
         self.shift_reg.set_buf_bit(self.bit_pos, state)
@@ -49,7 +64,7 @@ class Trigger(object):
         Включение объекта
         :return: none
         """
-        self.set_state(self.States.on)
+        self.__set_state(self.States.on)
         return
 
     def set_off(self):
@@ -57,15 +72,8 @@ class Trigger(object):
         Выключение объекта
         :return: none
         """
-        self.set_state(self.States.off)
+        self.__set_state(self.States.off)
         return
-
-    def get_state(self):
-        """
-        Получение текущего состояния переключателя
-        :return: значение типа Trigger.States
-        """
-        return Trigger.States(self.shift_reg.get_buf_bit(self.bit_pos))
 
     def apply_state(self):
         """
@@ -88,9 +96,71 @@ class Slider(object):
         opening = [1, 0]
         opened  = [1, 1]
 
+    def __init__(self, shift_reg, bit_plus, bit_minus, switch_time=1):
+        """
+        Конструктор
+        :param shift_reg: регистр, в котором хранится текущее состояние
+        :param bit_plus: позиция бита, который отвечает за положительный вывод мотора
+        :param bit_minus: позиция бита, который отвечает за отрицательный вывод мотора
+        :param switch_time: время переключения между двумя состояниями в секундах
+        :return: None
+        """
+        check_shift_reg_type(shift_reg)
 
+        if switch_time <= 0:
+            raise ValueError('switch_time must be bigger than zero')
 
-    pass
+        self.shift_reg = shift_reg
+        self.bit_plus = bit_plus
+        self.bit_minus = bit_minus
+        self.switch_time = switch_time
+
+    def get_state(self):
+        return self.States(
+            [
+                self.shift_reg.get_buf_bit(self.bit_plus),
+                self.shift_reg.get_buf_bit(self.bit_minus)
+            ]
+        )
+
+    def __set_state(self, state):
+        if type(state) != self.States:
+            raise ValueError('Type of state argument must be a Slider.State')
+
+        self.shift_reg.set_buf_bit(self.bit_plus, state[0])
+        self.shift_reg.set_buf_bit(self.bit_minus, state[1])
+
+    def __apply_state(self):
+        self.shift_reg.write_buffer()
+        return
+
+    def open(self):
+        if self.shift_reg.get_buf_bit(self.bit_plus) == 1:
+            pass
+
+        else:
+            self.__set_state(self.States.opening)
+            self.__apply_state()
+
+            time.sleep(self.switch_time)
+
+        self.__set_state(self.States.opened)
+
+        self.__apply_state()
+
+    def close(self):
+        if self.shift_reg.get_buf_bit(self.bit_plus) == 0:
+            pass
+
+        else:
+            self.__set_state(self.States.closing)
+            self.__apply_state()
+
+            time.sleep(self.switch_time)
+
+        self.__set_state(self.States.closed)
+
+        self.__apply_state()
 
 
 class Door(Slider):
