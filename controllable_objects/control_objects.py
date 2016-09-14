3709d016fbf5ebbc9965480898d913ba17afcebb
Sergey Kostyuk <contact@s-kostyuk.me>
2016-09-14 17:31:17 +0300

    Prototype of do_action function for T75

--- controllable_objects/control_objects.py
+++ controllable_objects/control_objects.py
@@ -8,6 +8,23 @@ from model import Model
 import logging
 
 
+##############################################################################################
+# FIXME List:
+# CC7 - Consider Change 7
+#   Метод do_action() ничего не возвращает. Возможно, следует этот факт изменить. Варианты:
+#   * выкидывать наружу возращаемое значение вызванного метода;
+#   * возвращать 0 при успехе и другие значения при ошибке;
+#   * оставить как есть, ничего не возвращать, выбрасывать исключения в случае ошибки.
+# CC8 - Consider Change 8
+#   Сейчас действия выполняются только над controllable'ами. Может разрешить выполнение
+#   действий и над другими объектами? Но тогда нужно реализовать TD2
+# TD1 - To Do 1
+#   Сделать полноценную реализацию метода do_action.
+# TD2 - To Do 2
+#   Сделать полноценную реализацию проверки разрешений на выполнение действия.
+##############################################################################################
+
+
 class ControlObjects(object):
 
     def __init__(self):
@@ -62,20 +79,47 @@ class ControlObjects(object):
         logging.debug("{0} destruction finished".format(self))
         return
 
-    def toggle_controllable(self, obj_id):
-        """
-        Функция для переключения объекта в противоположное состояние
-        :param obj_id: идентификатор объекта
-        :return: True - успешно, False - неуспешно
-        """
+    def __resolve_obj_by_id(self, obj_id: str):
         if type(obj_id) != str:
             raise ValueError('Value must be a string literal')
 
         if obj_id not in self.all_controllables:
             raise ValueError('id not found')
 
-        obj_alias = self.all_controllables[obj_id]
+        return self.all_controllables[obj_id]
+
+    def toggle_controllable(self, obj_id):
+        """
+        Функция для переключения объекта в противоположное состояние
+        :param obj_id: идентификатор объекта
+        :return: True - успешно, False - неуспешно
+        """
+        obj_alias = self.__resolve_obj_by_id(obj_id)
 
         obj_alias.toggle()
 
         return True
+
+    def check_action_permitted(self, obj_id: str, action: str, action_params):
+        # FIXME: TD2, Проверка прав на выполнение действия
+        logging.WARNING("Permission checking is not implemented")
+        pass
+
+    def do_action(self, obj_id: str, action: str, action_params=None):
+        """
+        Функция для выполнения действия на объекте
+        :param obj_id: тип объекта
+        :param action: действие, метод, который необходимо вызвать на объекте
+        :param action_params: параметры вызова
+        :return: None  # CC7
+        """
+        # FIXME: TD1
+        self.check_action_permitted(obj_id, action, action_params)
+
+        obj_alias = self.__resolve_obj_by_id(obj_id)
+
+        try:
+            method_to_call = getattr(obj_alias, action)
+            method_to_call(action_params)
+        except AttributeError:
+            raise
