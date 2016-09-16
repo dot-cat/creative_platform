865ba11223cf0e4bd2fd1809f16c09199a1aa22b
Sergey Kostyuk <contact@s-kostyuk.me>
2016-09-16 21:35:18 +0300

    Added argument checking and tests for MessagePattern

--- src/events/message_pattern.py
+++ src/events/message_pattern.py
@@ -1,5 +1,21 @@
+from collections.abc import Iterable
+
+
 class MessagePattern(object):
-    def __init__(self, type, source_list: list, event_list: list):
+    def __init__(self, type, source_list: Iterable, event_list: Iterable):
+        if not isinstance(source_list, Iterable):
+            raise ValueError("source_list must be iterable")
+
+        if not isinstance(event_list, Iterable):
+            raise ValueError("event_list must be iterable")
+
+        # Если список пустой...
+        if not source_list:
+            raise ValueError("source_list can't be empty")
+
+        if not event_list:
+            raise ValueError("event_list can't be empty")
+
         self.type = type
         self.sources = source_list
         self.events = event_list
---
+++ tests/unittests/events/test_message_pattern.py
@@ -0,0 +1,57 @@
+import unittest
+from copy import copy
+
+from events.message_pattern import MessagePattern
+
+valid_message_pattern_params = {
+    "type": "button",
+    "source_list": ["B1", "B2"],
+    "event_list": ["pressed"]
+}
+
+
+class TestMessagePatternInit(unittest.TestCase):
+    def test_normal_init(self):
+        mp = MessagePattern(
+            **valid_message_pattern_params
+        )
+
+    def test_init_not_iterable_sources(self):
+        invalid_params = copy(valid_message_pattern_params)
+
+        invalid_params["source_list"] = None
+
+        with self.assertRaisesRegex(ValueError, "source_list must be iterable"):
+            mp = MessagePattern(
+                **invalid_params
+            )
+
+    def test_init_not_iterable_events(self):
+        invalid_params = copy(valid_message_pattern_params)
+
+        invalid_params["event_list"] = None
+
+        with self.assertRaisesRegex(ValueError, "event_list must be iterable"):
+            mp = MessagePattern(
+                **invalid_params
+            )
+
+    def test_init_empty_sources(self):
+        invalid_params = copy(valid_message_pattern_params)
+
+        invalid_params["source_list"] = []
+
+        with self.assertRaisesRegex(ValueError, "source_list can't be empty"):
+            mp = MessagePattern(
+                **invalid_params
+            )
+
+    def test_init_empty_events(self):
+        invalid_params = copy(valid_message_pattern_params)
+
+        invalid_params["event_list"] = []
+
+        with self.assertRaisesRegex(ValueError, "event_list can't be empty"):
+            mp = MessagePattern(
+                **invalid_params
+            )
