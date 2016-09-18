4d812aab484569004aade5a0c536f876af3131c8
Sergey Kostyuk <contact@s-kostyuk.me>
2016-09-18 12:28:05 +0300

    Upadted names of variables

--- src/events/event_hub.py
+++ src/events/event_hub.py
@@ -10,34 +10,34 @@ class EventHub(object):
         sens_pattern = handler.get_sensitivity_list()
 
         # получаем список зарегистрированых типов событий в системе
-        types_available = self.handler_resolver
+        sources_by_type = self.handler_resolver
 
         # получаем список источников сообщений некоторого типа
-        sources_available = types_available.get(sens_pattern.type, dict())
+        events_by_source = sources_by_type.get(sens_pattern.type, dict())
 
         # для каждого источника сообщений...
         for source in sens_pattern.sources:
             # получаем список событий, которые с ним могут произойти
-            events_available = sources_available.get(source, dict())
+            handlers_by_event = events_by_source.get(source, dict())
 
             # для каждого события...
             for event in sens_pattern.events:
                 # получаем список его обработчиков
-                handlers_available = events_available.get(event, set())
+                handlers_available = handlers_by_event.get(event, set())
 
                 # добавляем обработчик для нашего изменения
                 handlers_available.add(handler)
 
-                # сохраняем новый список обработчиков
-                events_available[event] = handlers_available
+                # сохраняем новое множество обработчиков
+                handlers_by_event[event] = handlers_available
 
             # сохраняем новый словарь событий
-            sources_available[source] = events_available
+            events_by_source[source] = handlers_by_event
 
         # сохраняем новый словарь источников
-        types_available[sens_pattern.type] = sources_available
+        sources_by_type[sens_pattern.type] = events_by_source
 
-    def remove_handler(self, handler):
+    def remove_handler(self, handler: AbsHandler):
         # получаем список чувствительности
         sens_pattern = handler.get_sensitivity_list()
 
