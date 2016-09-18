86ac91a32d1e358423ae3288ef0c79ddd2bc9284
Sergey Kostyuk <contact@s-kostyuk.me>
2016-09-18 16:55:54 +0300

    Improved implementation of handler addition and removal

--- src/events/event_hub.py
+++ src/events/event_hub.py
@@ -1,11 +1,18 @@
+from enum import Enum
+
 from handlers.abs_handler import AbsHandler
+
+
+class Operation(Enum):
+    remove = 0
+    add = 1
 
 
 class EventHub(object):
     def __init__(self):
         self.handler_resolver = dict()
 
-    def add_handler(self, handler: AbsHandler):
+    def __process_handler(self, handler: AbsHandler, operation: Operation):
         # получаем список чувствительности
         sens_pattern = handler.get_sensitivity_list()
 
@@ -25,45 +32,23 @@ class EventHub(object):
                 # получаем список его обработчиков, существующий или новый
                 handlers_available = handlers_by_event.setdefault(event, set())
 
-                # добавляем обработчик для нашего изменения
-                handlers_available.add(handler)
+                # добавляем или удаляем обработчик для нашего события
+                if operation is Operation.add:
+                    handlers_available.add(handler)
+                else:
+                    handlers_available.remove(handler)
+
+    def add_handler(self, handler: AbsHandler):
+        self.__process_handler(handler, Operation.add)
 
     def remove_handler(self, handler: AbsHandler):
-        # получаем список чувствительности
-        sens_pattern = handler.get_sensitivity_list()
-
-        # получаем список зарегистрированых типов событий в системе
-        types_available = self.handler_resolver
-
-        # получаем список источников сообщений некоторого типа
-        sources_available = types_available.get(sens_pattern.type, dict())
-
-        # для каждого источника сообщений...
-        for source in sens_pattern.sources:
-            # получаем список событий, которые с ним могут произойти
-            events_available = sources_available.get(source, dict())
-
-            # для каждого события...
-            for event in sens_pattern.events:
-                # получаем список его обработчиков
-                handlers_available = events_available.get(event, set())
-
-                # удаляем обработчик для нашего изменения
-                handlers_available.remove(handler)
-
-                # сохраняем новый список обработчиков
-                events_available[event] = handlers_available
-
-            # сохраняем новый словарь событий
-            sources_available[source] = events_available
-
-        # сохраняем новый словарь источников
-        types_available[sens_pattern.type] = sources_available
+        self.__process_handler(handler, Operation.remove)
 
     def accept_event(self, message):
         types_available = self.handler_resolver
 
         sources_available = types_available.get(message.type)
+
         if sources_available is None:
             return
 
