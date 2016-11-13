cdc5d8ec1025998c9d5388170c4c9ea3b841575c
Sergey Kostyuk <contact@s-kostyuk.me>
2016-11-13 23:36:37 +0200

    Tiny improvements

--- src/connections/factories.py
+++ src/connections/factories.py
@@ -9,20 +9,25 @@ from connections.mpd_client import MPDClientConnection
 def get_connection_by_config(config) -> object:
     connection = None
 
-    if config["con_type"] == "shiftreg":
+    con_type = config["con_type"]
+
+    if con_type == "shiftreg":
         connection = \
             ShiftRegBuffered(
                 ShiftRegGPIO(**config["con_params"])
             )
-    elif config["con_type"] == "mpd_server":
+
+    elif con_type == "mpd_server":
         try:
             connection = \
                 MPDClientConnection(**config["con_params"])
         except ConnectionRefusedError:
             logging.warning("Unable to connect to MPD server {0}".format(config["con_params"]))
-    elif config["con_type"] == "serial":
+
+    elif con_type == "serial":
+        tty = config["con_params"]["tty"]
+
         try:
-            tty = config["con_params"]["tty"]
             connection = serial.Serial(tty)
             connection.baudrate = config["con_params"]["baudrate"]
         except serial.SerialException:
