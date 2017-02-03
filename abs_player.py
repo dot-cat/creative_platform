fe71a0a1312ea1d33795ad49d2118bbe0c9eef2e
Sergey Kostyuk <contact@s-kostyuk.me>
2017-02-03 20:02:18 +0200

    STEP 3.2: Separated base and specific things

---
+++ abs_player.py
@@ -0,0 +1,62 @@
+from enum import Enum
+
+from dpl.core.things import AbsThing
+
+
+class AbsPlayer(AbsThing):
+    """
+    Плеер. Объект, который проигрывает медиафайлы
+    """
+    class States(Enum):
+        """
+        Возможные состояния выдвигающегося элемента
+        """
+        playing = 0,
+        stopped = 1,
+        paused = 2,
+        undefined = None
+
+    def __init__(self, con_instance, con_params, metadata=None):
+        """
+        Конструктор, копия конструктора из базового класса
+        :param metadata:
+        """
+        super().__init__(con_instance, con_params, metadata)
+
+    def play(self) -> None:
+        raise NotImplementedError
+
+    def stop(self) -> None:
+        raise NotImplementedError
+
+    def pause(self) -> None:
+        raise NotImplementedError
+
+    def next(self) -> None:
+        raise NotImplementedError
+
+    def prev(self) -> None:
+        raise NotImplementedError
+
+    def get_current_track(self) -> dict:
+        raise NotImplementedError
+
+    def toggle(self) -> None:
+        """
+        Переключает состояние плеера в противоположное:
+        запускает остановленное воспроизведение и останавливает запущенное
+        :return None
+        """
+        curr_state = self.get_state()
+
+        # Если проигрывание остановлено...
+        if curr_state == self.States.stopped or curr_state == self.States.paused:
+            self.play()  # запускаем его
+
+        # Если проигрывание запущено...
+        elif curr_state == self.States.playing:
+            self.pause()  # приостанавливаем его
+
+        # Fixme CC1:
+        else:  # Если состояние другое...
+            pass  # игнорируем команду
---
+++ abs_thing.py
@@ -0,0 +1,81 @@
+##############################################################################################
+# FIXME List:
+# CC2 - Consider Change 2
+#   Методы не возвращают результат совего выполнения (успешно/неуспешно). Возможно, нужно 
+#   добавить индикацию статуса выполнения
+# CC12 - Consider Change 12
+#   Плохо переносится на код на С++
+##############################################################################################
+
+from enum import Enum
+
+
+class AbsThing(object):
+    """
+    Базовый абстрактный класс для всех контролируемых объектов
+    """
+
+    class States(Enum):
+        """
+        Возможные состояния объекта
+        """
+        on = True
+        off = False
+        undefined = None
+
+    def __init__(self, con_instance, con_params, metadata=None):
+        """
+        Инициализация контроллируемого объекта
+        :param con_instance: экземпляр connection'а
+        :param con_params: параметры доступа к connection'у
+        :param metadata: метеданные (имя, описание, тип объекта)
+        """
+        self.con_instance = con_instance
+        self.con_params = con_params
+        self.metadata = metadata
+
+        # Строку ниже добавляйте сами, если connections не обеспечивает буффер
+        # self.curr_state = self.States.undefined
+
+    def set_state(self, target_state):
+        """
+        Немедленно установить конкретное состояние
+        :param target_state: значение из self.States, желаемое состояние
+        """
+        raise NotImplementedError
+
+    def set_state_buffer(self, target_state):
+        """
+        Установить состояние в буффере, не отсылать в connection
+        :param target_state: значение из self.States, желаемое состояние
+        """
+        raise NotImplementedError
+
+    def apply_buffer_state(self):
+        """
+        Отослать состояние из буффера в connection
+        """
+        raise NotImplementedError
+
+    def get_state(self) -> States:
+        """
+        Вернуть свое состояние внешнему миру из буфера
+        :return: значение типа self.States
+        """
+        raise NotImplementedError
+
+    def get_state_string(self) -> str:
+        """
+        Вернуть свое состояние внешнему миру из буфера. Состояние возращается в виде строки
+        :return: str, текущее состояние
+        """
+        return str(self.get_state().name)  # Fixme: CC12
+
+    def toggle(self):
+        """
+        Переключить из текущего состояния в противоположное
+        """
+        raise NotImplementedError
+
+    def get_metadata(self):
+        return self.metadata
