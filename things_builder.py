b272918329153b750a2cef868531925d2c033591
Sergey Kostyuk <contact@s-kostyuk.me>
2017-02-04 22:02:52 +0200

    Moved log messages from root logger to dpl's loggers

--- things_builder.py
+++ things_builder.py
@@ -2,6 +2,8 @@ import logging
 
 from dpl.core.things import Thing, ThingRegistry, ThingFactory
 
+logger = logging.getLogger(__name__)
+
 
 def get_thing_by_params(con_instance, con_params, metadata) -> Thing:
     item_type = metadata["type"]
@@ -14,9 +16,9 @@ def get_thing_by_params(con_instance, con_params, metadata) -> Thing:
 
     if factory is None:
         if ThingRegistry.has_type(item_type):
-            logging.warning("Unsupported connection: {0}".format(con_instance))
+            logger.warning("Unsupported connection: {0}".format(con_instance))
         else:
-            logging.warning("Unknown type of object: {0}".format(item_type))
+            logger.warning("Unknown type of object: {0}".format(item_type))
         return None
     else:
         return factory.build(con_instance, con_params, metadata)
