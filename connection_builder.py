cf0b7a12eac8ebf4d3f090417d94941aa485738d
Sergey Kostyuk <contact@s-kostyuk.me>
2017-02-04 22:02:52 +0200

    Moved log messages from root logger to dpl's loggers

--- connection_builder.py
+++ connection_builder.py
@@ -3,12 +3,14 @@ import logging
 from dpl.core.connections import ConnectionRegistry
 from dpl.core.connections import Connection
 
+logger = logging.getLogger(__name__)
+
 
 def get_connection_by_config(config: dict) -> Connection:
     factory = ConnectionRegistry.resolve_factory(config["con_type"])
 
     if factory is None:
-        logging.warning("Unknown connection: %s", config)
+        logger.warning("Unknown connection: %s", config)
         return None
     else:
         return factory.build(config)
