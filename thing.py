1e6b115ceba63bcd3112d72d3e7fa3e7bb254329
Sergey Kostyuk <contact@s-kostyuk.me>
2017-02-05 21:38:36 +0200

    Draft of rethinked Thing

--- thing.py
+++ thing.py
@@ -1,72 +1,128 @@
 ##############################################################################################
 # FIXME List:
-# CC2 - Consider Change 2
-#   Методы не возвращают результат совего выполнения (успешно/неуспешно). Возможно, нужно 
-#   добавить индикацию статуса выполнения
-# CC12 - Consider Change 12
-#   Плохо переносится на код на С++
+#
 ##############################################################################################
 
 from enum import Enum
+
 from dpl.core.connections import Connection
 
 
 class Thing(object):
     """
-    Базовый абстрактный класс для всех контролируемых объектов
+    Базовый абстрактный класс для всех объектов системы ("вещей")
+
+    Гарантии:
+    * текущее состояние объекта храниться в буфере
+    * текущее состояние обновляется автоматически и всегда актуально
+    * состояние объекта не изменяется, если подключение потеряно
+    * при обновлении состояния объект оповещает об этом всех подписчиков
+    * объект обладает свойствами is_available и last_seen
+    * is_available обозначает готовность объекта к приему команд и/или
+      считыванию новых значений
+    * last_seen - время, когда объект был доступен
+    * обладает методами disable и enable
+    * disable переводит объект в состояние "недоступен" и
+      останавливает автоматическое обновление состояния
+    * enable инициирует восстановление соединения и, как следствие,
+      переводит объект в состояние "доступен"
+    * обладает свойством extended_info, в котором содержится расширенная
+      информация о текущем состоянии (например, текущее значение сенсора,
+      текущий трек и плейлист плеера и т.д.)
+    * extended_info может вернуть None
     """
 
     class States(Enum):
         """
-        Возможные состояния объекта
+        Возможные состояния объекта,
+        переопределяется в каждом из наследников
         """
-        on = True
-        off = False
-        undefined = None
+        unknown = None
 
-    def __init__(self, con_instance: Connection, con_params, metadata=None):
+    def __init__(self, con_instance: Connection, con_params: object, metadata: dict=None):
         """
-        Инициализация контроллируемого объекта
-        :param con_instance: экземпляр connection'а
-        :param con_params: параметры доступа к connection'у
-        :param metadata: метеданные (имя, описание, тип объекта)
+        Конструктор
+        :param con_instance: экземпляр соединения
+        :param con_params: параметры доступа к соединению
+        :param metadata: метаданные объекта (см. свойство metadata)
         """
-        self.con_instance = con_instance
-        self.con_params = con_params
-        self.metadata = metadata
+        self.__con_instance = con_instance
+        self.__con_params = con_params
+        self.__metadata = metadata
+        self.__on_update = None
+        self.__on_avail_update = None
 
-        # Строку ниже добавляйте сами, если connections не обеспечивает буффер
-        # self.curr_state = self.States.undefined
-
-    def set_state(self, target_state):
+    @property
+    def metadata(self) -> dict or None:
         """
-        Немедленно установить конкретное состояние
-        :param target_state: значение из self.States, желаемое состояние
+        Метаданные объекта. Например: ID, название, описание и т.д.
+        :return: словарь с метаданными либо None
+        """
+        return self.__metadata
+
+    @property
+    def state(self) -> States:
+        """
+        Текущее состояние объекта
+        :return: объект типа self.States
         """
         raise NotImplementedError
 
-    def get_state(self) -> States:
+    @property
+    def is_available(self) -> bool:
         """
-        Вернуть свое состояние внешнему миру из буфера
-        :return: значение типа self.States
-        """
-        raise NotImplementedError
-
-    def get_state_string(self) -> str:
-        """
-        Вернуть свое состояние внешнему миру из буфера. Состояние возращается в виде строки
-        :return: str, текущее состояние
-        """
-        return str(self.get_state().name)  # Fixme: CC12
-
-    def toggle(self):
-        """
-        Переключить из текущего состояния в противоположное
+        Доступность объекта для использования
+        :return: True - доступен, False - недоступен
         """
         raise NotImplementedError
 
-    def get_metadata(self):
-        return self.metadata
+    @property
+    def last_seen(self) -> float:
+        """
+        Возвращает время, когда объект был доступен в последний раз
+        :return: float, UNIX time
+        """
+        raise NotImplementedError
+
+    @property
+    def extended_info(self) -> dict or None:
+        """
+        Возвращает расширенную информацию о состоянии объекта
+        :return: словарь с информацией либо None
+        """
+        raise NotImplementedError
+
+    @property
+    def on_update(self) -> callable:
+        return self.__on_update
+
+    @on_update.setter
+    def on_update(self, func):
+        self.__on_update = func
+
+    @property
+    def on_avail_update(self) -> callable:
+        return self.__on_avail_update
+
+    @on_avail_update.setter
+    def on_avail_update(self, func):
+        self.__on_avail_update = func
+
+    def disable(self) -> None:
+        """
+        Отключает объект, останавливает обновление состояния и
+        делает его неактивным
+        :return: None
+        """
+        raise NotImplementedError
+
+    def enable(self) -> None:
+        """
+        Включает объект, запускает обновление состояние и делает
+        его активным
+        :return: None
+        """
+        raise NotImplementedError
 
 
 class ThingFactory(object):
