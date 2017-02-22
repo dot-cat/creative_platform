e0b028773bb70bb219f4d5fa4214d2c4fcb894f4
Sergey Kostyuk <contact@s-kostyuk.me>
2017-02-22 22:24:07 +0200

    Renamed commands to actions. Again.
    May be renamed back in the future

--- dpl/core/things/player.py
+++ dpl/core/things/player.py
@@ -26,7 +26,7 @@ class Player(Actuator):
         super().__init__(con_instance, con_params, metadata)
 
     @property
-    def commands(self) -> tuple:
+    def actions(self) -> tuple:
         """
         Возвращает список всех доступных команд
         :return: tuple
--- dpl/core/things/slider.py
+++ dpl/core/things/slider.py
@@ -36,7 +36,7 @@ class Slider(Actuator):
         super().__init__(con_instance, con_params, metadata)
 
     @property
-    def commands(self) -> tuple:
+    def actions(self) -> tuple:
         """
         Возвращает список всех доступных команд
         :return: tuple
