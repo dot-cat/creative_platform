96fd9762a6b7d1b30f18d2e6ed17c6b224e55276
Sergey Kostyuk <contact@s-kostyuk.me>
2017-02-22 23:41:50 +0200

    WIP: Draft of the MPDPlayer

--- dpl/specific/things/players/mpd_player.py
+++ dpl/specific/things/players/mpd_player.py
@@ -14,10 +14,15 @@
 
 import contextlib
 import logging
+import time
+import warnings
 
 from dpl.core.things import ThingFactory, ThingRegistry
+from dpl.core.things import Actuator
 from dpl.core.things import Player
 from dpl.specific.connections.mpd_client import MPDClientConnection
+
+import mpd
 
 logger = logging.getLogger(__name__)
 
@@ -29,9 +34,11 @@ class MPDPlayer(Player):
         :param metadata:
         """
         super().__init__(con_instance, con_params, metadata)
+        self._is_enabled = True
+        self._last_seen = time.time()
 
     @contextlib.contextmanager
-    def connection(self):
+    def _connection(self):
         try:
             self._con_instance.reconnect()
             yield
@@ -53,36 +60,109 @@ class MPDPlayer(Player):
     @property
     def state(self):
         try:
-            with self.connection():
+            with self._connection():
                 status = self._con_instance.status()
         except ConnectionRefusedError:
             return MPDPlayer.States.undefined
 
         return self.__mpd_state_to_self_state(status["state"])
 
-    def play(self) -> None:  # CC15
-        with self.connection():
+    @property
+    def is_available(self) -> bool:  # FIXME: Изменить реализацию, загрушка
+        """
+        Доступность объекта для использования
+        :return: True - доступен, False - недоступен
+        """
+        return self._is_enabled
+
+    def _update_last_seen(self):
+        self._last_seen = time.time()
+
+    @property
+    def last_seen(self) -> float:  # Fixme: CC23
+        """
+        Возвращает время, когда объект был доступен в последний раз
+        :return: float, UNIX time
+        """
+        if self._is_enabled:
+            self._update_last_seen()
+
+        return self._last_seen
+
+    @property
+    def extended_info(self):
+        """
+        Возвращает расширенную информацию о состоянии объекта
+        :return: словарь с информацией либо None
+        """
+        try:
+            with self._connection():
+                return self._con_instance.status()
+        except ConnectionRefusedError:
+            return None
+
+    def disable(self) -> None:
+        """
+        Отключает объект, останавливает обновление состояния и
+        делает его неактивным
+        :return: None
+        """
+        self._is_enabled = False
+
+        if self.on_avail_update:
+            self.on_avail_update(self)
+
+    def enable(self) -> None:
+        """
+        Включает объект, запускает обновление состояние и делает
+        его активным
+        :return: None
+        """
+        self._is_enabled = True
+
+        if self.on_avail_update:
+            self.on_avail_update(self)
+
+    def play(self) -> Actuator.ExecutionResult:  # CC15
+        with self._connection():
             self._con_instance.play()
 
-    def stop(self) -> None:  # CC15
-        with self.connection():
+        return Actuator.ExecutionResult.OK
+
+    def stop(self) -> Actuator.ExecutionResult:  # CC15
+        with self._connection():
             self._con_instance.stop()
 
-    def pause(self) -> None:  # CC15
-        with self.connection():
+        return Actuator.ExecutionResult.OK
+
+    def pause(self) -> Actuator.ExecutionResult:  # CC15
+        with self._connection():
             self._con_instance.pause()
 
-    def next(self) -> None:  # CC15
-        with self.connection():
-            self._con_instance.next()
+        return Actuator.ExecutionResult.OK
 
-    def prev(self) -> None:  # CC15
-        with self.connection():
-            self._con_instance.previous()
+    def next(self) -> Actuator.ExecutionResult:  # CC15
+        with self._connection():
+            try:
+                self._con_instance.next()
+            except mpd.CommandError:  # Треки нельзя переключать тогда, когда плеер MPD остановлен
+                return self.ExecutionResult.IGNORED_BAD_STATE
+
+    def prev(self) -> Actuator.ExecutionResult:  # CC15
+        with self._connection():
+            try:
+                self._con_instance.previous()
+            except mpd.CommandError:  # Треки нельзя переключать тогда, когда плеер MPD остановлен
+                return self.ExecutionResult.IGNORED_BAD_STATE
 
     def get_current_track(self) -> dict:  # CC16
+        warnings.warn(
+            "This method will be removed in v0.4. "
+            "All needed information is moved to extended_info attribute",
+            PendingDeprecationWarning
+        )
         try:
-            with self.connection():
+            with self._connection():
                 return self._con_instance.currentsong()  # CC 17
         except ConnectionRefusedError:
             return {
