2e4401fe8c21acc179dc017680f19d32200520fe
Sergey Kostyuk <contact@s-kostyuk.me>
2017-03-01 00:32:13 +0200

    Revert "Pylint fix: renamed logger constant"
    This reverts commit d95773ea56706649f102455c546d7967b495b45f.

--- connection_builder.py
+++ connection_builder.py
@@ -3,14 +3,14 @@ import logging
 from dpl.core.connections import ConnectionRegistry
 from dpl.core.connections import Connection
 
-LOGGER = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 def get_connection_by_config(config: dict) -> Connection or None:
     factory = ConnectionRegistry.resolve_factory(config["con_type"])
 
     if factory is None:
-        LOGGER.warning("Unknown connection: %s", config)
+        logger.warning("Unknown connection: %s", config)
         return None
     else:
         return factory.build(config)
