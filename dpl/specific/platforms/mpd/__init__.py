69cabb344f32a0c567bcce2cecfcb3b4e621306c
Sergey Kostyuk <contact@s-kostyuk.me>
2017-08-16 09:52:55 +0300

    Specific implementations of thing platforms are moved the separate repo

--- dpl/specific/platforms/mpd/__init__.py
+++
@@ -1,4 +0,0 @@
-from .mpd_client import MPDClientConnection
-from .mpd_player import MPDPlayer
-
-__all__ = ["MPDClientConnection", "MPDPlayer"]
--- dpl/specific/platforms/shift_reg_gpio/__init__.py
+++
@@ -1,5 +0,0 @@
-from .shift_reg_gpio_buffered import ShiftRegGPIOBuffered
-from .shift_reg_slider import ShiftRegSlider
-from .shift_reg_trigger import ShiftRegTrigger
-
-__all__ = ["ShiftRegGPIOBuffered", "ShiftRegSlider", "ShiftRegTrigger"]
